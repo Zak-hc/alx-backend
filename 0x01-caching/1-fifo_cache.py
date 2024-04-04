ed04f2251c0c197871c0fa6d5ed48e159dc42b6a
Zak-hc <zhaci001@gmail.com>
2024-04-04 02:36:58 +0000

    all task

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+class FIFOCache(BaseCaching):
+    """
+    inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        initization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        keys = list(self.cache_data.keys())
+        if len(keys) > BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(keys[0]))
+            del self.cache_data[keys[0]]
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
